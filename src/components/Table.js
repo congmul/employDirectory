78e032e9540b6a0ec5b2a07d5bfff1c4bce7e4af
Jehyun <congmul@hotmail.com>
2021-01-23 00:12:27 -0800

    Add all information about members from API

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TableHeader = () => {
     return (
-        <thead style={{fontWeight:"700"}}>
+        <thead style={{ fontWeight: "700" }}>
             <tr>
                 <th>Image</th>
                 <th>Name</th>
@@ -15,32 +15,72 @@ const TableHeader = () => {
 }
 
 const TableBody = (props) => {
+    console.log("props in Table Body");
+    console.log(props.members);
+    props.members.map(object => console.log(object))
+    
     return (
-        <tbody className="">
-            <tr>
-                <td>Image</td>
-                <td>Jim Hudson</td>
-                <td>(631)-892-9752</td>
-                <td>jum.hudson@example.com</td>
-                <td>09-17-1995</td>
-            </tr>
-            <tr>
-                <td>Image</td>
-                <td>Jim Hudson</td>
-                <td>(631)-892-9752</td>
-                <td>jum.hudson@example.com</td>
-                <td>09-17-1995</td>
-            </tr>
+        <tbody>
+            {props.members.map(object =>
+                <tr>
+                    <td className="align-middle"><img src={object.picture.medium} alt="profile" /></td>
+                    <td className="align-middle">{object.name.first} {object.name.last}</td>
+                    <td className="align-middle">{object.cell}</td>
+                    <td className="align-middle">{object.email}</td>
+                    <td className="align-middle">{object.dob.date}</td>
+                </tr>
+            )}
         </tbody>
     )
 }
 
+class Table extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            members: []
+        };
+    }
 
-export default function Table() {
-    return (
-        <table className="table table-hover" style={{"textAlign":"center"}}>
-            <TableHeader />
-            <TableBody />
-        </table>
-    )
+    componentDidMount() {
+        fetch("https://randomuser.me/api/?results=50&nat=us")
+            .then(res => res.json())
+            .then((result) => {
+                console.log("result.results");
+                console.log(result.results);
+                this.setState({
+                    isLoaded: true,
+                    members: result.results
+                })
+            },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    })
+                })
+    }
+
+
+    render() {
+        const { error, isLoaded, members } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            console.log("members in Table render()")
+            console.log(members);
+            return (
+                <table className="table table-hover" style={{ "textAlign": "center" }}>
+                    <TableHeader />
+                    <TableBody members={members} />
+                </table>
+            )
+        }
+    }
 }
+
+export default Table
