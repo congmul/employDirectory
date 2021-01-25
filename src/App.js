8a7e489da2f03c40b79d63fdb3a8f7be0963a1cf
Jehyun <congmul@hotmail.com>
2021-01-25 12:37:19 -0800

    Add Search form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
 import Table from './components/Table'
+import Search from './components/Search'
 
 function App() {
     return (
         <div>
             <Header />
+            <Search />
             <div style={{"margin": "5rem"}}>
                 <Table />
             </div>
---
+++ src/components/Search.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+class Search extends Component{
+    render(){
+        return(
+            <div className="container text-center">
+            <input type="search" placeholder="Search" />
+            </div>
+        )
+    }
+}
+
+export default Search;
