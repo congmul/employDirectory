bf176cd52be94ae6d70a585a163afdf865f9984d
Jehyun <congmul@hotmail.com>
2021-01-25 15:02:42 -0800

    Modified some style

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,19 @@
 import React from "react";
 
+const style = {
+    marginBottom: "50px",
+    width: "50%",
+    searchForm : {
+        padding:"10px",
+        width:"100%"
+    }
+}
 
 function Search(props){
         return(
             <>
-            <div className="container text-center">
-            <input type="search" name="members" value={props.value} onChange={props.handleInputChange} placeholder="Search" />
+            <div className="container text-center" style={style}>
+            <input type="search" name="members" style={style.searchForm} value={props.value} onChange={props.handleInputChange} placeholder="Search" />
             </div>
             </>
         )
